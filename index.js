bc2dce7ae3ff06bc2d8220f6d3c43315115145e9
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-07-14 13:19:47 +0200

    Some swim tests

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 let assign = require('object.assign')
+let Swim   = require('swim')
+let mcpeer = require('multicast-dns-peer')
 
-let yolog = function(opts) {
-    if (!(this instanceof yolog)) return new yolog(opts)
-    if (!opts) opts = {}
-    this.log = []
-    this.listeners = []
+let yolog = function(log, opts) {
+    if (!(this instanceof yolog)) return new yolog(log, opts)
+    this.opts = assign({
+        bootstrapPeers : []
+    }, opts || {})
+
+    // MULTICAST - discover peers to bootstrap swim
+    this.mcpeer = mcpeer(log, {
+        answers : [
+            { name:'yolog', type:'SRV', data: { port:11000, target:'yolog' }}
+        ]
+    })
+    this.mcpeer.on('peer', function(peer) {
+        // TODO: utilize pickPeer
+        var srv = peer.filter(function(p) { return p.type == 'SRV' })
+        if (srv.lenght > 0) this.opts.bootstrapPeers.push(srv)
+        if (this.opts.bootstrapPeers.length > 0) {
+            var addrs = this.opts.bootstrapPeers.map(function(srv) {
+                return srv.data.target+':'+srv.data.port
+            })
+            console.log('bootstrapping swim',addrs)
+            //this.swim.bootstrap(addrs)
+        } 
+    }.bind(this))
+
+    // SWIM
+    this.swim = new Swim({
+        local : {
+            host : '127.0.0.1:11000'
+        }
+    })
+
+    this.swim.on(Swim.EventType.Error, function onError(err) {
+        console.log('swim error', err)
+    });
+    this.swim.on(Swim.EventType.Ready, function onReady() {
+        console.log('swim ready')
+    });
+
+    // errorhandling, retry
+ 
 }
-yolog.prototype = assign(Array.prototype, {
-    append : function(data) {
-        this.log.push(data)
-        this.listeners.forEach((l) => {
-            if (l.pattern == '*') l.handler(data)
-            if (data.indexOf(l.pattern) >= 0) l.handler(data)
-        })
-    },
-    get : function(index, cb) {
-        cb(this.log[index])
-    },
-    replay : function(index, handler, end) {
-        while(index < this.log.length) { handler(this.log[index]); index++ }
-        end(this.log.length)
-    },
-    on : function(pattern, handler) {
-        this.listeners.push({ handler : handler, pattern : pattern })
-    }
-})
 
 module.exports = yolog
--- test/spec.js
+++ test/spec.js
@@ -1,50 +1,6 @@
 let assert = require('assert')
-let yolog  = require('..')
+let yolog  = require('../index')
 
-module.exports = (constructor, afterEachCb) => {
-    describe('yolog', () => {
-
-        afterEach((done) => {
-            if (typeof afterEachCb == 'function') afterEachCb(done)
-            else done()
-        })
-
-        it.only('can append data', (done) => {
-            let log = constructor() 
-            let data = 'some arbitrary data'
-            log.append(data)
-            setTimeout(() => {
-                log.get(0, (_data) => {
-                    assert(data == _data)
-                    done()
-                })
-            },100)
-        })
-
-        it('can replay events', (done) => {
-            let log = constructor() 
-            log.append('event1')
-            log.append('event2')
-            let _events = []
-            setTimeout(() => {
-                log.replay(0, (e) => {
-                    _events.push(e)
-                }, (err) => {
-                    assert(_events.length == 2)
-                    done()
-                })
-            },100)
-        })
-
-        it('emits events', (done) => {
-            let log = constructor() 
-            log.on('*', (e) => {
-                assert(e == 'some data')
-                done()
-            })
-            log.append('some data')
-        })
-    })
-}
-
-module.exports(function() { return new yolog() })
+it('can use multicast-dns to bootstrap swim', function(done) {
+    
+})
