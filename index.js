5576ad5ae8802a24ddc389c83fe5ecbeb8d31131
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-07-14 23:45:23 +0200

    pubsub > bus

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-let EventEmitter = require('events').EventEmitter
-let assign       = require('object.assign')
-let address      = require('network-address')
-let Swim         = require('swim')
-let mcpeer       = require('multicast-dns-peer')
+var EventEmitter = require('events').EventEmitter
+var assign       = require('object.assign')
+var address      = require('network-address')
+var Swim         = require('swim')
+var mcpeer       = require('multicast-dns-peer')
+var nano         = require('nanomsg')
 
-let yolog = function(log, opts) {
+var yolog = function(log, opts) {
     if (!(this instanceof yolog)) return new yolog(log, opts)
     this.opts = assign({
         bootstrapPeers : [],
         port           : 11000,
-        address        : address()
+        address        : address(),
+        busport        : 11010,
+        busaddress     : address()
     }, opts || {})
+    this.subs = {}
+
+    // BUS (PUB)
+    this.bus = nano.socket('pub')
+    this.bus.bind(this.busaddr())
 
     // MULTICAST - discover peers to bootstrap swim
     this.mcpeer = mcpeer(log, {
@@ -26,7 +34,6 @@ let yolog = function(log, opts) {
         ]
     })
     this.mcpeer.on('peer', function(peer) {
-        // TODO: utilize pickPeer
         var srv = peer.filter(function(p) { return p.type == 'SRV' })
         if (srv.length > 0) 
             this.opts.bootstrapPeers = this.opts.bootstrapPeers.concat(srv)
@@ -43,29 +50,37 @@ let yolog = function(log, opts) {
     this.swim = new Swim({
         local : {
             host : this.opts.address+':'+this.opts.port,
-            meta : {} 
+            meta : {
+                bus : this.busaddr() 
+            } 
         }
     })
 
     this.swim.on(Swim.EventType.Error, function(err) {
-        console.error('swim error', err)
+//        console.error('swim error', err)
     });
     this.swim.on(Swim.EventType.Change, function(data) {
-        console.error('swim change', data)
-    });
+//        console.error('swim change', data)
+        this.checkBusMembers()
+    }.bind(this));
     this.swim.on(Swim.EventType.Ready, function onReady() {
         this.emit('ready', this.swim.members())
         this.destroyMcPeer()
+        this.checkBusMembers()
     }.bind(this));
 
-    // errorhandling, retry
+    // errorhandling, retry, etc.
  
 }
 
 yolog.prototype = assign({
     destroy : function() {
-        this.swim.leave()
         this.destroyMcPeer()
+        this.swim.leave()
+        this.bus.close()
+        Object.keys(this.subs).forEach(function(membus) {
+            this.subs[membus].close()
+        }.bind(this))
     },
     destroyMcPeer : function() {
         try {
@@ -74,6 +89,23 @@ yolog.prototype = assign({
             if (e.message == 'Not running') return
             throw e
         }
+    },
+    busaddr : function() {
+        return this.opts.bus || ('tcp://'+this.opts.busaddress+':'+this.opts.busport)
+    },
+    checkBusMembers : function() {
+        // Add new members
+        this.swim.members().forEach(function(mem) {
+            var membus = mem.meta.bus
+            if (Object.keys(this.subs).indexOf(membus) >= 0) return
+            var sub = nano.socket('sub')
+            sub.connect(membus)
+            sub.on('message', function(msg) {
+                console.log('got bus msg', msg.toString(), this.opts.bus)
+            }.bind(this))
+            this.subs[membus] = sub
+        }.bind(this))
+        // TODO: Remove dead/suspect members
     }
 }, EventEmitter.prototype)
 
--- test/spec.js
+++ test/spec.js
@@ -2,23 +2,41 @@ let assert = require('assert')
 let yolog  = require('../index')
 
 it('can use multicast-dns to bootstrap swim', function(done) {
-    let log1 = yolog('yolog')
-    log1.on('ready', (peer) => {
-        var ports = peer.map(function(p) {
+    this.timeout(5000)
+    let log1 = yolog('yolog', {
+        bus : 'ipc:///tmp/yolog1.ipc'
+//        bus : 'tcp://127.0.0.1:11010'
+    })
+    log1.on('ready', (peers) => {
+        assert(peers.length == 2)
+        var ports = peers.map(function(p) {
             return p.host.split(':')[1]
         })
         assert(ports.indexOf('11001') >= 0)
-        assert(ports.indexOf('11002') >= 0)
-        log1.destroy()
-        log2.destroy()
-        log3.destroy()
-        done()
+        assert(ports.indexOf('11002') >= 0)   
+
+        log1.bus.send('yolo')
+        log2.bus.send('yolo2')
+
+        setTimeout(() => {
+            log1.destroy()
+            log2.destroy()
+            log3.destroy()
+            done()
+        },1000)
     })
     let log2 = yolog('yolog', {
-        port : 11001
+        port    : 11001,
+        bus : 'ipc:///tmp/yolog2.ipc'
+//        bus : 'tcp://127.0.0.1:11011'
     })
     let log3 = yolog('yolog', {
-        port : 11002
+        port    : 11002,
+        bus : 'ipc:///tmp/yolog3.ipc'
+//        bus : 'tcp://127.0.0.1:11012'
     })
 })
 
+//it('creates a bus and distributes events to all nodes', function(done) {
+
+//})
