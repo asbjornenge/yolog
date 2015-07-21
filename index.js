5c902b54893d89ad11a89d0072a1dbfed34e8f8b
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-07-21 08:54:39 +0200

    Some testing

--- index.js
+++ index.js
@@ -4,120 +4,72 @@ var address      = require('network-address')
 var Swim         = require('swim')
 var mcpeer       = require('multicast-dns-peer')
 var nano         = require('nanomsg')
+var emitStream   = require('emit-stream')
 
-var defaultOpts = {
+//var log = yolog('mylog')
+//
+// log.projector
+//      .reduce(function() {})
+//      .map(function() {})
+//
+// log.livestream
+//      .reduce(function() {})
+//      .map(function() {})
+//
+// log.store
+//      .filter(function() {})
+//
+// stdin.pipe(log)
+//
+// log.kill()
+
+var stream = require('stream')
+var util   = require('util')
+var emitStream = require('emit-stream')
+
+var _opts = {
     autojoin   : true,
-    port       : 11000,
-    address    : address(),
-    pubPort    : 11010,
-    pubAddress : address()
+    memberaddr : 'tcp://'+address()+':'+11000,
+    pubaddr    : 'inproc://yolog'
 }
 
-var yolog = {
-    join : function(fn) {
-        join(this.opts, function(err, swim) {
-            if (err) throw err
-            this.emit('ready')
-            this.swim = swim
-            if (typeof fn === 'function') fn()
-        }.bind(this))
-    },
-    members : function() {
-        return this.swim.members()
-    },
-    leave : function() {
-        if (this.swim) this.swim.leave()
-        delete this.swim
-        this._leave && this._leave()
-    },
-    destroy : function() {
-        this.leave()
+var yolog = function(log, opts) {
+    if (!(this instanceof yolog)) return new yolog(log, opts)
+    this.opts = assign(_opts, opts || {}, { log:log }) 
+    this.state = {
+        stores       : [],
+        projectors   : [],
+        livestreams  : [],
+        swarm        : null,
+        connections  : {
+            pubs : [nano.socket('pub')],
+            subs : []
+        }
     }
+    this.pub = this.state.connections.pubs[0]
 }
+util.inherits(yolog, stream.Duplex)
+//yolog.prototype._write = function(chunk, encoding, done) {
+//    // TODO: pub.send ?
+//    console.log('write', chunk.toString())
+//    this.state.stores.concat(this.state.livestreams).forEach(function(stream) {
+//        
+//    })
+//    done()
+//}
+//yolog.prototype._read = function(n) {
+//    // TODO: sub.onMessage ?
+//    setTimeout(function() {
+//        this.push('some data\n')
+//        this.pause()
+//    }.bind(this),100)
+//}
 
-var producer = function(log, opts) {
-    if (!(this instanceof producer)) return new producer(log, opts)
-    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'producer' }) 
-    this.logs = []
-    this.pub  = nano.socket('pub')
-    this.pub.bind(pubaddr(this.opts))
-    if (this.opts.autojoin) this.join(this.init.bind(this))
-}
-producer.prototype = assign({
-    init : function() {
-        this.swim.on('error', function(err) { console.error('swim error', err) })
-    },
-    append : function(data) {
-        this.pub.send(data)
-// TODO:        this.logs.push(data)
-    },
-    _leave : function() {
-        // TODO: This throws is no connection - why?
-        this.pub.close()
-    }
-}, yolog, EventEmitter.prototype)
+module.exports = yolog
 
-var store = function(log, opts) {
-    if (!(this instanceof store)) return new store(log, opts)
-    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'store' })
-    this.subs = {}
-    this.pub  = nano.socket('pub')
-    this.pub.bind(pubaddr(this.opts))
-    if (this.opts.autojoin) this.join(this.init.bind(this))
-}
-store.prototype = assign({
-    init : function() {
-        this.swim.on('change', this.manageSubscriptions.bind(this))
-        this.swim.on('update', this.manageSubscriptions.bind(this))
-        this.swim.on('error', function(err) { console.error('swim error', err) })
-        this.manageSubscriptions()
-    },
-    manageSubscriptions : function() {
-        manageSubscriptions(this, this.swim.members(), 'producer')
-    },
-    onLog : function(log) {
-        this.emit('log', log)
-    },
-    forward : function(log) {
-        this.pub.send(log)
-    },
-    _leave : function() {
-        this.pub.close()
-        Object.keys(this.subs).forEach(function(pubaddr) {
-            this.subs[pubaddr].close()
-        }.bind(this))
-        this.subs = {}
-    } 
-}, yolog, EventEmitter.prototype)
+// Transformations
 
-var projector = function(log, opts) {
-    if (!(this instanceof projector)) return new projector(log, opts)
-    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'projector' }) 
-    this.subs = {}
-    if (this.opts.autojoin) this.join(this.init.bind(this))
-}
-projector.prototype = assign({
-    init : function() {
-        this.swim.on('change', this.manageSubscriptions.bind(this))
-        this.swim.on('update', this.manageSubscriptions.bind(this))
-        this.swim.on('error', function(err) { console.error('swim error', err) })
-        this.manageSubscriptions()
-    },
-    manageSubscriptions : function() {
-        manageSubscriptions(this, this.swim.members(), 'store')
-    },
-    onLog : function(log) {
-        this.emit('log', log)
-    },
-    _leave : function() {
-        Object.keys(this.subs).forEach(function(pubaddr) {
-            this.subs[pubaddr].close()
-        }.bind(this))
-        this.subs = {}
-    } 
-}, yolog, EventEmitter.prototype)
-
-function join(opts, fn) {
+function joinswarm(opts, fn) {
     var peers = []
     var swim = gossip(opts) 
     var mc = multicast(opts)
@@ -134,94 +86,215 @@ function join(opts, fn) {
     })
 }
 
-function multicast(opts) {
-    return mcpeer(opts.log, {
-        answers : [
-            { 
-                name:'yolog', 
-                type:'SRV', 
-                data: { 
-                    port:opts.port, 
-                    target:opts.address 
-                }
-            }
-        ]
-    })
-}
-
-function gossip(opts) {
-    return new Swim({
-        local : {
-            host : opts.address+':'+opts.port,
-            meta : typemeta(opts) 
-        }
-    })
-}
-
-function typemeta(opts) {
-    switch(opts.type) {
-        case 'producer':
-            return {
-                type : 'producer',
-                pub  : pubaddr(opts) 
-            }
-        case 'store':
-            return {
-                type : 'store',
-                pub  : pubaddr(opts) 
-            }
-        case 'projector':
-            return {
-                type : 'projector'
-            }
-        default:
-            return {
-                type : 'virus'
-            }
-    }
-}
-
-function pubaddr(opts) {
-    return opts.produceraddr || 'tcp://'+opts.pubAddress + ':' + opts.pubPort
-}
-
-function manageSubscriptions(yolog, members, filter) {
-    var producers = members.filter(function(member) {
-        return member.meta.type == filter
-    })
-    var pubs = producers.map(function(member) {
-        return member.meta.pub
-    })
-   
-    // Connect to new producers 
-
-    producers 
-        .filter(function(producer) {
-            return Object.keys(yolog.subs).indexOf(producer.meta.pub) < 0 
-        })
-        .forEach(function(producer) {
-            var pub = producer.meta.pub
-            var sub = nano.socket('sub')
-            sub.connect(pub)
-            sub.on('message', yolog.onLog.bind(yolog))
-            yolog.subs[pub] = sub
-        })
-
-    // Disconnect lost producers
-
-    Object.keys(yolog.subs)
-        .filter(function(pub) {
-            return pubs.indexOf(pub) < 0 
-        })
-        .forEach(function(producer) {
-            yolog.subs[producer].close()
-            delete yolog.subs[producer]
-        })
-        
-}
-
-module.exports = {
-    producer  : producer,
-    store     : store,
-    projector : projector
-} 
+//var yolog = {
+//    join : function(fn) {
+//        join(this.opts, function(err, swim) {
+//            if (err) throw err
+//            this.emit('ready')
+//            this.swim = swim
+//            if (typeof fn === 'function') fn()
+//        }.bind(this))
+//    },
+//    members : function() {
+//        return this.swim.members()
+//    },
+//    leave : function() {
+//        if (this.swim) this.swim.leave()
+//        delete this.swim
+//        this._leave && this._leave()
+//    },
+//    destroy : function() {
+//        this.leave()
+//    }
+//}
+//
+//var producer = function(log, opts) {
+//    if (!(this instanceof producer)) return new producer(log, opts)
+//    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'producer' }) 
+//    this.logs = []
+//    this.pub  = nano.socket('pub')
+//    this.pub.bind(pubaddr(this.opts))
+//    if (this.opts.autojoin) this.join(this.init.bind(this))
+//}
+//producer.prototype = assign({
+//    init : function() {
+//        this.swim.on('error', function(err) { console.error('swim error', err) })
+//    },
+//    append : function(data) {
+//        this.pub.send(data)
+//// TODO:        this.logs.push(data)
+//    },
+//    _leave : function() {
+//        // TODO: This throws is no connection - why?
+//        this.pub.close()
+//    }
+//}, yolog, EventEmitter.prototype)
+//
+//var store = function(log, opts) {
+//    if (!(this instanceof store)) return new store(log, opts)
+//    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'store' })
+//    this.subs = {}
+//    this.pub  = nano.socket('pub')
+//    this.pub.bind(pubaddr(this.opts))
+//    if (this.opts.autojoin) this.join(this.init.bind(this))
+//}
+//store.prototype = assign({
+//    init : function() {
+//        this.swim.on('change', this.manageSubscriptions.bind(this))
+//        this.swim.on('update', this.manageSubscriptions.bind(this))
+//        this.swim.on('error', function(err) { console.error('swim error', err) })
+//        this.manageSubscriptions()
+//    },
+//    manageSubscriptions : function() {
+//        manageSubscriptions(this, this.swim.members(), 'producer')
+//    },
+//    onLog : function(log) {
+//        this.emit('log', log)
+//    },
+//    forward : function(log) {
+//        this.pub.send(log)
+//    },
+//    _leave : function() {
+//        this.pub.close()
+//        Object.keys(this.subs).forEach(function(pubaddr) {
+//            this.subs[pubaddr].close()
+//        }.bind(this))
+//        this.subs = {}
+//    } 
+//}, yolog, EventEmitter.prototype)
+//
+//var projector = function(log, opts) {
+//    if (!(this instanceof projector)) return new projector(log, opts)
+//    this.opts = assign(defaultOpts, opts || {}, { log:log, type:'projector' }) 
+//    this.subs = {}
+//    if (this.opts.autojoin) this.join(this.init.bind(this))
+//}
+//projector.prototype = assign({
+//    init : function() {
+//        this.swim.on('change', this.manageSubscriptions.bind(this))
+//        this.swim.on('update', this.manageSubscriptions.bind(this))
+//        this.swim.on('error', function(err) { console.error('swim error', err) })
+//        this.manageSubscriptions()
+//    },
+//    manageSubscriptions : function() {
+//        manageSubscriptions(this, this.swim.members(), 'store')
+//    },
+//    onLog : function(log) {
+//        this.emit('log', log)
+//    },
+//    _leave : function() {
+//        Object.keys(this.subs).forEach(function(pubaddr) {
+//            this.subs[pubaddr].close()
+//        }.bind(this))
+//        this.subs = {}
+//    } 
+//}, yolog, EventEmitter.prototype)
+//
+//function join(opts, fn) {
+//    var peers = []
+//    var swim = gossip(opts) 
+//    var mc = multicast(opts)
+//    mc.on('peer', function(peer) {
+//        peers = peers.concat(peer.filter(function(p) { return p.type == 'SRV' }))
+//        if (peers.length <= 2) return
+//        var addrs = peers.map(function(srv) {
+//            return srv.data.target+':'+srv.data.port
+//        })
+//        mc.destroy()
+//        swim.bootstrap(addrs, function(err) {
+//            fn(err, swim)
+//        })
+//    })
+//}
+//
+//function multicast(opts) {
+//    return mcpeer(opts.log, {
+//        answers : [
+//            { 
+//                name:'yolog', 
+//                type:'SRV', 
+//                data: { 
+//                    port:opts.port, 
+//                    target:opts.address 
+//                }
+//            }
+//        ]
+//    })
+//}
+//
+//function gossip(opts) {
+//    return new Swim({
+//        local : {
+//            host : opts.address+':'+opts.port,
+//            meta : typemeta(opts) 
+//        }
+//    })
+//}
+//
+//function typemeta(opts) {
+//    switch(opts.type) {
+//        case 'producer':
+//            return {
+//                type : 'producer',
+//                pub  : pubaddr(opts) 
+//            }
+//        case 'store':
+//            return {
+//                type : 'store',
+//                pub  : pubaddr(opts) 
+//            }
+//        case 'projector':
+//            return {
+//                type : 'projector'
+//            }
+//        default:
+//            return {
+//                type : 'virus'
+//            }
+//    }
+//}
+//
+//function pubaddr(opts) {
+//    return opts.produceraddr || 'tcp://'+opts.pubAddress + ':' + opts.pubPort
+//}
+//
+//function manageSubscriptions(yolog, members, filter) {
+//    var producers = members.filter(function(member) {
+//        return member.meta.type == filter
+//    })
+//    var pubs = producers.map(function(member) {
+//        return member.meta.pub
+//    })
+//   
+//    // Connect to new producers 
+//
+//    producers 
+//        .filter(function(producer) {
+//            return Object.keys(yolog.subs).indexOf(producer.meta.pub) < 0 
+//        })
+//        .forEach(function(producer) {
+//            var pub = producer.meta.pub
+//            var sub = nano.socket('sub')
+//            sub.connect(pub)
+//            sub.on('message', yolog.onLog.bind(yolog))
+//            yolog.subs[pub] = sub
+//        })
+//
+//    // Disconnect lost producers
+//
+//    Object.keys(yolog.subs)
+//        .filter(function(pub) {
+//            return pubs.indexOf(pub) < 0 
+//        })
+//        .forEach(function(producer) {
+//            yolog.subs[producer].close()
+//            delete yolog.subs[producer]
+//        })
+//        
+//}
+//
+//module.exports = {
+//    producer  : producer,
+//    store     : store,
+//    projector : projector
+//} 
--- test/spec.js
+++ test/spec.js
@@ -1,56 +1,33 @@
-var assert = require('assert')
-var Swim   = require('swim')
-var yolog  = require('../index')
+var assert  = require('assert')
+var Swim    = require('swim')
+var resumer = require('resumer')
+var yolog   = require('../index')
 
-describe('yolog', function() {
+function createStream(str) {
+    var stream = resumer()
+    stream.queue(str)
+    return stream
+}
 
-    var log1 = yolog.producer('yolog', {
-        port    : 11000, // default
-        pubPort : 11010  // default
-    })
-    var log2 = yolog.store('yolog', {
-        port    : 11001,
-        pubPort : 11011
-    })
-    var log3 = yolog.projector('yolog', {
-        port : 11002
-    })
+describe('yolog basics', function() {
 
-    before(function(done) {
-        log1.on('ready', done)
-    })
+    var log = yolog('mylog')
 
-    after(function() {
-        log1.destroy()
-        log2.destroy()
-        log3.destroy()
+    it('one can pipe/stream data into yolog', function() {
+        var s = createStream('yolo\n')
+        s.pipe(log)
+        assert(true)
     })
 
-    it('can use multicast-dns to bootstrap swim', function(done) {
-        var peers = log1.members()
-        assert(peers.length == 2)
-        var ports = peers.map(function(p) {
-            return p.host.split(':')[1]
-        })
-        assert(ports.indexOf('11001') >= 0)
-        assert(ports.indexOf('11002') >= 0)   
-        done()
-    })
-
-    it('has proper message flow producer -> store -> projector', function(done) {
-        var msg = 'some stringified data'
-        log1.append(msg)
-        log2.on('log', function(log) {
-            assert(log.toString() == msg)
-            // perform actual storage here
-            log2.forward(log.toString()+' stored')
-        })
-        log3.on('log', function(log) {
-            assert(log.toString() == msg+' stored')
-            done()
-        })
-    })
-
+//    it('returns observables', function(done) {
+//        var s = createStream('yolo\n')
+//        s.pipe(log)
+//        log.stream
+//            .forEach(function(buf) {
+//                console.log(buf.toString())
+//                done()
+//            })
+//    })
     // it can detect new members
 
 })
